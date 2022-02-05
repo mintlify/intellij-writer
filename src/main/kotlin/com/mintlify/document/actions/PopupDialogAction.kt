b1b359d28bfe7e255ca4228fb6db3138820f02d0
Han Wang <hyw2@cornell.edu>
2022-02-05 14:53:40 -0800

    Add setup for displaying comments

--- src/main/kotlin/com/mintlify/document/actions/PopupDialogAction.kt
+++ src/main/kotlin/com/mintlify/document/actions/PopupDialogAction.kt
@@ -3,18 +3,34 @@ package com.mintlify.document.actions;
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.CaretModel
 import com.intellij.openapi.editor.Editor
-
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.Project
 import com.mintlify.document.helpers.getDocFromApi
+
 
 public class PopupDialogAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val project: Project = e.getRequiredData(CommonDataKeys.PROJECT)
+        val document: Document = editor.document
+
         val caretModel: CaretModel = editor.getCaretModel()
         val selectedText = caretModel.currentCaret.selectedText ?: "";
+        val start = caretModel.currentCaret.selectionStart;
 
-        val docstring = getDocFromApi(selectedText, "testingID");
-        println(docstring);
+        val languageId = e.getData(LangDataKeys.PSI_FILE)?.language?.displayName?.lowercase();
+        val context = document.text;
+
+        val response = getDocFromApi(selectedText, "testingID", languageId, context);
+        if (response != null) {
+            val insertDocstring = response.docstring + '\n';
+            WriteCommandAction.runWriteCommandAction(project) {
+                document.replaceString(start, start, insertDocstring)
+            }
+        }
     }
 }
--- src/main/kotlin/com/mintlify/document/helpers/api.kt
+++ src/main/kotlin/com/mintlify/document/helpers/api.kt
@@ -3,10 +3,12 @@ package com.mintlify.document.helpers
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpPost
 import com.google.gson.Gson
+import com.beust.klaxon.Klaxon
 
 data class RequestBody(
     var userId: String,
     var code: String,
+    var languageId: String?,
     var context: String,
     var commented: Boolean,
     var email: String,
@@ -16,24 +18,29 @@ data class RequestBody(
     // TODO: Add location
 )
 
+data class Response(
+    var docstring: String,
+)
+
 fun getDocFromApi(
     code: String,
     userId: String,
+    languageId: String?,
     context: String = code,
     commented: Boolean = true,
     email: String = "",
     docStyle: String = "Auto-detect",
     source: String = "intellij",
     width: Int = 80
-): String? {
-
-    val body = RequestBody(userId, code, context, commented, email, docStyle, source, width);
+): Response? {
+    val body = RequestBody(userId, code, languageId, context, commented, email, docStyle, source, width);
     val (_, _, result) = "http://localhost:5000/docs/write/v2".httpPost()
         .jsonBody(Gson().toJson(body).toString())
         .responseString()
     val (payload, _) = result
     if (payload != null) {
-        return payload;
+        val parsed = Klaxon().parse<Response>(payload)
+        return parsed;
     }
 
     return null;
