73b0404369feda9c4fded65e776a888b98d0d193
Hahnbee Lee <hl985@cornell.edu>
2022-02-07 20:27:51 -0800

    idk im sad

--- src/main/kotlin/com/mintlify/document/ui/MyToolWindow.kt
+++ src/main/kotlin/com/mintlify/document/ui/MyToolWindow.kt
@@ -8,30 +8,14 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 
 class MyToolWindow(toolWindow: ToolWindow) {
-    private val refreshToolWindowButton: JButton? = null
     private val hideToolWindowButton: JButton? = null
-    private val currentDate: JLabel? = null
-    private val timeZone: JLabel? = null
     private val myToolWindowContent: JPanel? = null
 
     init {
         hideToolWindowButton?.addActionListener { _ -> toolWindow.hide(null) }
-        refreshToolWindowButton?.addActionListener { _ -> currentDateTime() }
-        currentDateTime()
     }
 
-    fun currentDateTime() {
-        // Get current date and time
-        val instance: Calendar = Calendar.getInstance()
-        currentDate?.text = "March 12, 2000"
-        val min: Int = instance.get(Calendar.MINUTE)
-        val strMin = if (min < 10) "0$min" else min
-        // Get time zone
-        val gmt_Offset: Int = instance.get(Calendar.ZONE_OFFSET) // offset from GMT in milliseconds
-        var str_gmt_Offset: String = (gmt_Offset / 3600000).toString()
-        str_gmt_Offset = if (gmt_Offset > 0) "GMT + $str_gmt_Offset" else "GMT - $str_gmt_Offset"
-        timeZone?.text = str_gmt_Offset
-    }
+
 
     val content: JPanel?
         get() = myToolWindowContent
